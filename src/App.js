ed0a0de3a2cf84832a7f55026fe132bcc6a69182
kelechi-nwankpa <nnunnunnu90@gmail.com>
2021-10-05 15:53:35 +0100

    added cartIcon and cartDropdown components to our application while also implementing cart reducer

--- src/App.js
+++ src/App.js
@@ -66,10 +66,8 @@ const mapStateToProps =({ user }) =>({
     currentUser: user.currentUser
 })
 
-const mapDispatchToProps = (dispatch) => {
-  return {
+const mapDispatchToProps = (dispatch) => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
-  }
-}
+})
 
 export default connect( mapStateToProps, mapDispatchToProps )(App);
